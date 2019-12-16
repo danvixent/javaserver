2d9e507e34983bd22c69956d4233db22611ac6bc
Daniel Oluojomu <51540572+danvixent@users.noreply.github.com>
2019-12-16 14:00:05 +0100

    Add files via upload
    Nothing special needed to run this code though :-)

---
+++ server.java
@@ -0,0 +1,53 @@
+package task;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+class StaticFileServer implements HttpHandler {
+
+    public static void main(String args[]) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(InetAddress.getLoopbackAddress(), 8082), 0);
+        server.start();
+        // ... more server contexts
+        server.createContext("/", new StaticFileServer());
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String fileId = exchange.getRequestURI().getPath();
+        fileId = "C:\\" + fileId;
+        File file = getFile(fileId);
+        if (file == null) {
+            String response = "Error 404 File not found.";
+            exchange.sendResponseHeaders(404, response.length());
+            OutputStream output = exchange.getResponseBody();
+            output.write(response.getBytes());
+            output.flush();
+            output.close();
+        } else {
+            exchange.sendResponseHeaders(200, 0);
+            OutputStream output = exchange.getResponseBody();
+            FileInputStream fs = new FileInputStream(file);
+            final byte[] buffer = new byte[0x10000];
+            int count = 0;
+            while ((count = fs.read(buffer)) >= 0) {
+                output.write(buffer, 0, count);
+            }
+            output.flush();
+            output.close();
+            fs.close();
+        }
+    }
+
+    private File getFile(String fileId) {
+        File fl = new File(fileId);
+        return fl;
+    }
+}
